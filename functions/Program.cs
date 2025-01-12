7fef5c57c070bf12aca80df7de6899cfe5c1a0fa
Shalak Anastasia <nastiashalak@gmail.com>
2025-01-12 13:48:42 +0200

    add validation

--- functions/Program.cs
+++ functions/Program.cs
@@ -4,6 +4,10 @@
 
 (T[], T[]) SplitArray<T>(T[] array, int index)
 {
+    if (array == null)
+        throw new ArgumentNullException(nameof(array), "Array can't be null.");
 
+    if (index < 0 || index > array.Length)
+        throw new ArgumentOutOfRangeException(nameof(index), "Index is out of the bounds of the array.");
 }
 
